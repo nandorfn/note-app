b55eeae001f2c17ba0b815da6abd2d2ae07e92ed
nandorfn <firnandorfn@gmail.com>
2023-08-12 13:31:19 +0700

    create Navbar

---
+++ app/components/NavbarMenu.tsx
@@ -0,0 +1,18 @@
+
+const NavbarMenu: React.FC = () => {
+  const listMenu: string[][] = [
+    ['Home', 'Logo'],
+    ['Notes', 'Logo'],
+    ['To-Do', 'Logo'],
+    ['Trash', 'Logo'],
+    ['Settings', 'Logo']
+  ]
+
+    return (
+        <>
+          
+        </>
+    );
+};
+
+export default NavbarMenu;
