8330957c8ac45741410d553f7516168e23a83bfc
nandorfn <firnandorfn@gmail.com>
2023-08-17 23:42:57 +0700

    add input form

--- app/create/note/page.tsx
+++ app/create/note/page.tsx
@@ -1,12 +1,9 @@
-'use client'
-
-
 const page: React.FC = () => {
     return (
         <>
-          <form>
-            <textarea placeholder="Title" />
-            <textarea placeholder="Content" />
+          <form className="flex flex-col mx-10 p-8 bg-white rounded-lg shadow-sm ">
+            <textarea className=" border-b-2 border-slate-100 resize-none focus:outline-none" placeholder="Title" />
+            <textarea className="resize-none focus:outline-none pt-4" placeholder="Content" />
           </form>
         </>
     );
