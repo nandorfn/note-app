db5ffc331fc932a39d4c56ca751e0a24ef284194
nandorfn <firnandorfn@gmail.com>
2023-08-20 23:17:32 +0700

    update: add menu icon when a note clicked

--- app/components/Note.tsx
+++ app/components/Note.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { sanitize } from 'dompurify';
 import { useState } from "react";
 
 interface NoteProps {
@@ -21,12 +22,21 @@ const Note: React.FC<NoteProps> = ({ notes }) => {
         <div
           onClick={() => handleActive(note.id)}
           key={note.id}
-          className={`bg-green-200 rounded-xl h-80 p-4 basis-1/2 ${
-            activeNoteId === note.id ? "border-2 border-black" : ""
-          }`}
+          className={`bg-green-200 rounded-xl h-64 p-4 basis-1/2 overflow-hidden relative ${activeNoteId === note.id ? "border-2 border-black" : ""
+            }`}
         >
-          <h2 className="text-2xl">{note.title}</h2>
-          <p>{note.content}</p>
+          <h2 className="lg:text-2xl">{note.title}</h2>
+          <p className='break-all truncate' dangerouslySetInnerHTML={{ __html: note.content.replace(/\n/g, "<br>") }} />
+          {
+            activeNoteId === note.id
+              ? <div className={`absolute left-0 bottom-0 pb-4 w-full bg-green-200`}>
+                <div className='mx-4 flex justify-end gap-4'>
+                  <i className="fa-solid fa-pen"></i>
+                  <i className="fa-solid fa-trash-can"></i>
+                </div>
+              </div>
+              : null
+          }
         </div>
       ))}
     </>
--- app/components/NoteContainer.tsx
+++ app/components/NoteContainer.tsx
@@ -12,7 +12,7 @@ const NoteContainer: React.FC<NoteProps> = ({notes}) => {
     <>
       <div className="flex flex-col gap-10 bg-white rounded-xl shadow-sm p-8 h-auto w-auto">
         <h1 className="text-4xl font-medium ">Notes</h1>
-        <div className="grid grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-4">
           <Note notes={notes}/>
         </div>
       </div>
