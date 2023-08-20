a764c5ab249aa61a449e583bda052874284704c2
nandorfn <firnandorfn@gmail.com>
2023-08-20 22:35:28 +0700

    update: add border effect when note clicked

--- app/components/Note.tsx
+++ app/components/Note.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useState } from "react";
+
 interface NoteProps {
   notes: {
     title: string;
@@ -6,18 +9,28 @@ interface NoteProps {
   }[];
 }
 const Note: React.FC<NoteProps> = ({ notes }) => {
-    return (
-        <>
-          {notes.map(note => (
-            <div key={note.id} className="bg-green-200 rounded-xl h-80 p-4 basis-1/2 ">
-            <h2 className="text-2xl">{note.title}</h2>
-            <p>{note.content}</p>
-          </div>
-          ))
-          
-          }
-        </>
-    );
+  const [activeNoteId, setActiveNoteId] = useState<number | null>(null);
+
+  const handleActive = (noteId: number): void => {
+    setActiveNoteId((prevNoteId) => (prevNoteId === noteId ? null : noteId));
+  };
+
+  return (
+    <>
+      {notes.map((note) => (
+        <div
+          onClick={() => handleActive(note.id)}
+          key={note.id}
+          className={`bg-green-200 rounded-xl h-80 p-4 basis-1/2 ${
+            activeNoteId === note.id ? "border-2 border-black" : ""
+          }`}
+        >
+          <h2 className="text-2xl">{note.title}</h2>
+          <p>{note.content}</p>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Note;
