caaa629c002681187bb20134fa135780c356e4e1
nandorfn <firnandorfn@gmail.com>
2023-08-21 12:16:20 +0700

    update: NoteMenu with better semantic html

--- app/components/NoteMenu.tsx
+++ app/components/NoteMenu.tsx
@@ -7,21 +7,32 @@ interface NoteProps {
   activeNoteId: number | null;
 }
 
-const NoteMenu: React.FC<NoteProps> = ({note, activeNoteId }) => {
-    return (
-        <>
-          {
-            activeNoteId === note.id
-              ? <div className={`absolute left-0 bottom-0 pb-4 w-full bg-green-200`}>
-                <div className='mx-4 flex justify-end gap-4'>
-                  <i className="fa-solid fa-pen"></i>
-                  <i className="fa-solid fa-trash-can"></i>
-                </div>
-              </div>
-              : null
-          }
-        </>
-    );
+const NoteMenu: React.FC<NoteProps> = ({ note, activeNoteId }) => {
+
+  const btnColor = {
+    red: {
+      color: '#ff2222'
+    }
+  }
+
+  return (
+    <>
+      {
+        activeNoteId === note.id
+          ? <div className='absolute right-2 bottom-0 pb-6 w-full bg-green-200'>
+            <div className='mx-4 flex justify-end gap-6'>
+              <button type="button">
+                <i className="fa-solid fa-pen fa-lg cursor-pointer"></i>
+              </button>
+              <button>
+                <i className="fa-solid fa-trash-can fa-lg cursor-pointer" style={btnColor.red}></i>
+              </button>
+            </div>
+          </div>
+          : null
+      }
+    </>
+  );
 };
 
 export default NoteMenu;
