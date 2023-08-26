aa78704c9c1b0bde33c54ddfda537859797ceae8
nandorfn <firnandorfn@gmail.com>
2023-08-26 21:43:56 +0700

    update: note (add border when clicked)

--- app/to-do/components/Todo.tsx
+++ app/to-do/components/Todo.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { format } from 'date-fns';
+import { useState } from 'react';
 interface TodosProps {
   todos: {
     id: number;
@@ -11,12 +13,21 @@ interface TodosProps {
   }[];
 }
 const Todo: React.FC<TodosProps> = ({ todos }) => {
+  const [activeTodoId, setActiveTodoId] = useState<number|null>(null);
+  
+  const handleActive = (noteId: number): void => {
+    setActiveTodoId((prevTodoId) => (prevTodoId === noteId ? null : noteId));
+  };
+
+
   return (
     <>
       {todos.map((todo) => (
         <article 
           key={todo.id} 
-          className='flex flex-col relative bg-yellow-300 h-56 basis-1/2 p-4 rounded-3xl'>
+          onClick={() => handleActive(todo.id)}
+          className={`flex flex-col relative bg-yellow-300 h-56 basis-1/2 p-4 rounded-3xl ${activeTodoId === todo.id ? "border-2 border-black" : ""
+        }`}>
           <p className='text-sm'>{todo.deadline ? format(new Date(todo.deadline), 'dd MMM yyyy') : 'N/A'}</p>
           
           <h2 className='text-4xl py-2'>{todo.title}</h2>
